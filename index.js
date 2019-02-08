40a0ed008cbe30d4903842db7a6c83b23f23af10
Gilbert Torchon <gilbert@noukod.com>
2019-02-08 16:26:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,9 @@ class BoardMember{
     return "You will see great things from Scuber."
   }
 
+  sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained at ${this.training}.`
+  }
+
 
 }
